80255a3c5f83683cb80699c24319028dddaebf27
Patrick van Loon <p.j.vanloon1991@gmail.com>
2019-09-22 13:40:09 +0200

    Initial commit.

---
+++ src/StrictTreeNodeTrait.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Aeviiq\TreeNode;
+
+use Aeviiq\TreeNode\Exception\InvalidArgumentException;
+
+trait StrictTreeNodeTrait
+{
+    use TreeNodeTrait {
+        setParent as private setTreeNodeParent;
+        addChild as private addTreeNodeChild;
+    }
+
+    public function setParent(?TreeNodeInterface $parent): void
+    {
+        if (null !== $parent) {
+            $this->guardTreeNodeType($parent);
+        }
+
+        $this->setTreeNodeParent($parent);
+    }
+
+    public function addChild(TreeNodeInterface $child): void
+    {
+        $this->guardTreeNodeType($child);
+        $this->addTreeNodeChild($child);
+    }
+
+    /**
+     * @return string The class|interface name of the allowed instance.
+     */
+    abstract protected function getAllowedInstance(): string;
+
+    /**
+     * @throws InvalidArgumentException When the given tree node is not of the allowed instance.
+     */
+    private function guardTreeNodeType(TreeNodeInterface $treeNode): void
+    {
+        if (!\is_a($treeNode, $allowed = $this->getAllowedInstance())) {
+            throw new InvalidArgumentException(\sprintf('Tree node must be of type "%s", "%s" given.', $allowed, \get_class($treeNode)));
+        }
+    }
+}
---
+++ src/TreeNodeInterface.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Aeviiq\TreeNode;
+
+interface TreeNodeInterface
+{
+    public function isRoot(): bool;
+
+    public function getRoot(): TreeNodeInterface;
+
+    public function isLeaf(): bool;
+
+    public function getParent(): ?TreeNodeInterface;
+
+    public function setParent(?TreeNodeInterface $parent): void;
+
+    public function getChildren(): TreeNodeCollection;
+
+    public function addChild(TreeNodeInterface $child): void;
+
+    public function hasChild(TreeNodeInterface $child): bool;
+
+    public function removeChild(TreeNodeInterface $child): void;
+
+    public function clearChildren(): void;
+}
